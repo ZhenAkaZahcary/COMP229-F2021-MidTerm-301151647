b3a476edd1a92ffc7e60f421247c50e1bea2cdce
Zhen Xu <zachar1xu@gmail.com>
2021-10-29 11:53:48 -0400

    Add Button Fixed

--- server/routes/books.js
+++ server/routes/books.js
@@ -25,10 +25,8 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    res.render('books/details', {title: "Add a Book", books: ''});
+    
 
 });
 
